ae2538495ec8e6266f1000a279cd1fd6f91e580b
f4r3n <guillaume_k@hotmail.fr>
2025-08-18 10:03:46 +0200

    fix mobile responsiveness

--- src/lib/about.svelte
+++ src/lib/about.svelte
@@ -1,14 +1,23 @@
 <script>
+  import devImage from "$lib/assets/dev_2.jpg";
   const linkedinURL =
     "https://www.linkedin.com/in/guillaume-kotulski-008a11103/";
 </script>
 
-<main>
-  <div class="row-adaptive full-height">
-    <div class="left-half"></div>
-    <div class="column half right-half">
-      <div class="t1 upper top">Guillaume Kotulski</div>
-      <p>
+<main class="h-full w-full">
+  <div class="flex flex-col md:flex-row h-full">
+    <!-- Left half -->
+    <div
+      class="md:w-1/2 h-64 md:h-full bg-center bg-cover"
+      style="background-image: url({devImage});"
+    ></div>
+
+    <!-- Right half -->
+    <div class="md:w-1/2 flex flex-col px-5 py-6">
+      <h1 class="text-xl font-semibold uppercase mt-2 mb-4">
+        Guillaume Kotulski
+      </h1>
+      <p class="leading-8 text-left whitespace-pre-line">
         7 years of experience as a Software Engineer But how did I get there?...
         I learnt to code in high school. Back then, I used to write my code with
         a pen & paper during lessons and after school I transferred all of it to
@@ -17,79 +26,13 @@
         things I learn I am always eager to share it with friends and
         colleagues. Outside of work I am a passionate cyclist. You can find me
         learning Japanese, coding on personal projects, discovering new board
-        games! Feel free to reach out on <a href={linkedinURL} target="_blank"
-          >LinkedIn</a
+        games! Feel free to reach out on
+        <a
+          href={linkedinURL}
+          target="_blank"
+          class="text-blue-600 hover:underline">LinkedIn</a
         >
       </p>
     </div>
   </div>
 </main>
-
-<style>
-  .right-half {
-    max-width: 100%;
-    padding-left: 20px;
-  }
-
-  p {
-    line-height: 2em;
-    text-align: left;
-    padding-left: 15px;
-    max-width: 95%;
-    white-space: pre-line; /* collapse WS, preserve LB */
-  }
-
-  .left-half {
-    background-image: url($lib/assets/dev_2.jpg); /* change back to whatever background image you want */
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    flex: 0 0 50%; /* flex-basis: 50% */
-  }
-
-  .top {
-    margin-top: 10px;
-  }
-
-  main {
-    height: 100%;
-  }
-
-  .upper {
-    text-transform: uppercase;
-  }
-
-  .row-adaptive {
-    display: flex;
-    flex-direction: row;
-  }
-
-  .half {
-    flex: 0 0 50%;
-    height: 100%;
-  }
-  @media only screen and (max-width: 800px) {
-    .half {
-      flex: 0 0 50%;
-      height: 50%;
-    }
-
-    .row-adaptive {
-      display: flex;
-      flex-direction: column;
-    }
-
-    p {
-      max-width: 95%;
-    }
-  }
-
-  .column {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .full-height {
-    height: 100%;
-  }
-</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,35 +8,76 @@
     path: string;
   }
 
-  let nav: Menu[] = [
+  const nav: Menu[] = [
     { name: "About", path: "/" },
     { name: "Projects", path: "/projects" },
     { name: "CV", path: "/cv.pdf" },
   ];
-
   let currentNav = $derived(nav.find((menu) => menu.path == page.route.id));
 </script>
 
-<main class="h-screen flex">
-  <!-- Left menu -->
-  <aside class="w-48 bg-base-200 p-4">
-    <ul class="menu rounded-box">
-      {#each nav as n, i}
+<div class="drawer">
+  <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <!-- Navbar -->
+    <div class="navbar bg-base-300 w-full">
+      <div class="flex-none lg:hidden">
+        <label
+          for="my-drawer-3"
+          aria-label="open sidebar"
+          class="btn btn-square btn-ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block h-6 w-6 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div class="mx-2 flex-1 px-2">Guillaume Kotulski</div>
+      <div class="hidden flex-none lg:block">
+        <ul class="menu menu-horizontal">
+          <!-- Navbar menu content here -->
+          {#each nav as n}
+            <li>
+              <a
+                href={n.path}
+                class="btn btn-ghost justify-start w-full"
+                class:btn-active={currentNav?.name === n.name}
+              >
+                {n.name}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+    {@render children()}
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"
+    ></label>
+    <ul class="menu bg-base-200 min-h-full w-80 p-4">
+      <!-- Sidebar content here -->
+      {#each nav as n}
         <li>
-          <button
+          <a
+            href={n.path}
             class="btn btn-ghost justify-start w-full"
             class:btn-active={currentNav?.name === n.name}
-            onclick={() => {
-              goto(n.path);
-            }}
           >
             {n.name}
-          </button>
+          </a>
         </li>
       {/each}
     </ul>
-  </aside>
-  <section class="flex-1 p-6">
-    {@render children()}
-  </section>
-</main>
+  </div>
+</div>
